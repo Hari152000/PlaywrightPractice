0c8c8ceb5578a2c0b14adba344ee4ebf7dcaf274
Hari152000 <hariharancd1502@gmail.com>
2026-05-29 15:28:12 +0530

    modified

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,7 +1,7 @@
 import{test}from '@playwright/test'
+
 test('validate automation site',async({page})=>{
+    
     await page.goto('https://www.instagram.com/')
-    await page.locator('//input[@name="email"]').fill('hariharan')
-    await page.locator('//input[@name="pass"]').fill('Password123')
-    await page.locator('//span[text()="Log in"]').click()
+    
 })
